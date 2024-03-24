e62e312583f960f7b4b3915519ef425a98051663
soft <m.el.2001@mail.ru>
2024-03-24 20:52:03 +0500

    dz15

---
+++ dz15.py
@@ -0,0 +1,4 @@
+str1 = 'Я изучаю Nython. Мне нравится Nython. Nython очень интересный язык программирования.'
+str2 = 'Я изучаю Python. Мне нравится Python. Python очень интересный язык программирования.'
+str3 = str2.replace('p', 'n')
+print(str3)
