b720065a04ffac2c1243f7381b45fd3aec34abe6
soft <m.el.2001@mail.ru>
2024-08-11 15:45:01 +0500

    portfolio

--- second/portfolio/skills/models.py
+++ second/portfolio/skills/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Skills(models.Model):
+    objects = None
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='main/images/')
